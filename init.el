43591839e74338fe25239f14cb9920c392db45f7
Digvijay Patankar <dbpatankar@gmail.com>
2023-01-03 03:17:18 +0530

    org-ref with default cite type

--- init.el
+++ init.el
@@ -8,6 +8,8 @@
 (global-display-line-numbers-mode 1)
 (setq display-line-numbers-type 'relative)
 
+(global-visual-line-mode 1)
+
 (global-auto-revert-mode 1)
 
 (setq
@@ -53,24 +55,26 @@
   (evil-leader/set-leader "<SPC>")
   ;; KEYBINDINGS
   (evil-leader/set-key
-  "f f" 'counsel-find-file
-  "f r" 'counsel-recentf
-  "b b" 'counsel-switch-buffer
-  "b d" 'kill-this-buffer
-  "h v" 'counsel-describe-variable
-  "h f" 'counsel-describe-function
-  "h o" 'counsel-describe-symbol
-  "g g" 'magit
-  "w v" 'evil-window-vsplit
-  "w w" 'evil-window-next
-  "w c" 'evil-window-delete
-  )
+    "f f" 'counsel-find-file
+    "f r" 'counsel-recentf
+    "b b" 'counsel-switch-buffer
+    "b d" 'kill-this-buffer
+    "h v" 'counsel-describe-variable
+    "h f" 'counsel-describe-function
+    "h o" 'counsel-describe-symbol
+    "g g" 'magit
+    "w v" 'evil-window-vsplit
+    "w w" 'evil-window-next
+    "w c" 'evil-window-delete
+    )
   (evil-leader/set-key-for-mode 'org-mode
-    "m e" 'org-export-dispatch)
+    "m e" 'org-export-dispatch
+    "m @" 'org-ref-insert-cite-link
+    "m !" 'org-ref-insert-ref-link)
   (evil-leader/set-key-for-mode 'julia-mode
     "c c" 'julia-repl-send-region-or-line
     "c b" 'julia-repl-send-buffer)
-)
+  )
 
 
 (use-package evil
@@ -183,10 +187,88 @@
 
 (use-package org
   ;;:ensure t
+  :config
+  ;;(variable-pitch-mode nil)
+  (setq org-latex-listings 'minted) ;; or t
+  (add-to-list 'org-latex-packages-alist '("" "minted")) ;; or listings
+  (add-to-list 'org-latex-packages-alist '("" "xcolor"))
+  (setq bibtex-dialect 'biblatex)
+  (setq  org-latex-pdf-process '("latexmk -shell-escape -%latex -bibtex -interaction=nonstopmode -f -pdf %f"))
+  (with-eval-after-load 'ox-latex
+    (add-to-list 'org-latex-classes
+		 '("book-nodefaults"
+		   "\\documentclass{book}"
+		   ("\\chapter{%s}" . "\\chapter*{%s}")
+		   ("\\section{%s}" . "\\section*{%s}")
+		   ("\\subsection{%s}" . "\\subsection*{%s}")
+		   ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+		   ))
+    (add-to-list 'org-latex-classes
+		 '("exam"
+		   "\\documentclass{exam}"
+		   ("\\chapter{%s}" . "\\chapter*{%s}")
+		   ("\\section{%s}" . "\\section*{%s}")
+		   ("\\subsection{%s}" . "\\subsection*{%s}")
+		   ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+		   ))
+    (add-to-list 'org-latex-classes
+		 '("article-nodefaults"
+		   "\\documentclass{article}"
+		   ("\\section{%s}" . "\\section*{%s}")
+		   ("\\subsection{%s}" . "\\subsection*{%s}")
+		   ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+		   )))
+
+  ;; (add-hook 'org-mode-hook #'org-inline-pdf-mode)
+  (add-hook 'org-mode-hook
+	    (lambda () (add-hook 'after-save-hook #'org-babel-tangle
+				 :append :local)))
+  (add-hook 'org-mode-hook 'toc-org-mode)
   )
 
 (use-package toc-org
   :ensure t)
+
+(use-package org-superstar
+  :ensure t
+  :custom
+  (org-superstar-headline-bullets-list '("◉" "○" "✿" "🞛" "✜" "◆" "▶" "✸" "☯" "☯" "☯" "☯" "☯" "☯" ))
+  :config
+  (set-face-attribute 'org-level-8 nil :weight 'bold :inherit 'default)
+  ;; ;; Low levels are unimportant = no scaling
+  ;; (set-face-attribute 'org-level-7 nil :inherit 'org-level-8)
+  ;; (set-face-attribute 'org-level-6 nil :inherit 'org-level-8)
+  ;; (set-face-attribute 'org-level-5 nil :inherit 'org-level-8)
+  ;; (set-face-attribute 'org-level-4 nil :inherit 'org-level-8)
+  ;; ;; Top ones get scaled the same as in LaTeX (\large, \Large, \LARGE)
+  (set-face-attribute 'org-level-3 nil :inherit 'org-level-8 :height 1.2) ;\large
+  (set-face-attribute 'org-level-2 nil :inherit 'org-level-8 :height 1.44) ;\Large
+  (set-face-attribute 'org-level-1 nil :inherit 'org-level-8 :height 1.728) ;\LARGE
+  ;; ;; Only use the first 4 styles and do not cycle.
+  ;; (org-cycle-level-faces nil)
+  ;; (org-n-level-faces 4)
+  ;; ;; Document Title, (\huge)
+  ;; (set-face-attribute 'org-document-title nil
+  ;;                 :height 2.074
+  ;;                 :foreground 'unspecified
+  ;;                 :inherit 'org-level-8)
+  :hook (org-mode . org-superstar-mode)
+  )
+
+(use-package org-ref
+  :ensure t
+  :after org
+  ;; :defer t
+  :init
+  ;;(require 'org-ref-helm)
+  (require 'org-ref-ivy)
+  (setq org-ref-default-citation-link "autocite")
+  :config
+  (setq bibtex-dialect 'biblatex)
+  :bind
+  (("C-c c" . org-ref-insert-cite-link)
+   ("C-c r" . org-ref-insert-ref-link))
+  )
 
 (add-to-list 'load-path "/usr/share/asymptote/")
 (add-to-list 'load-path "/home/digvijay/emacs_custom_libs/")
@@ -223,7 +305,7 @@
   :ensure t
   :init
   (elpy-enable)
-  ;; Use IPython for REPL
+  ;; Use jupyter for REPL
   ;;  (setq python-shell-interpreter "jupyter"
   ;;	python-shell-interpreter-args "console --simple-prompt"
   ;;	python-shell-prompt-detect-failure-warning nil)
@@ -232,11 +314,10 @@
   ;; Use IPython for REPL
   (setq python-shell-interpreter "ipython"
 	python-shell-interpreter-args
-	"-i --simple-prompt --InteractiveShell.display_page=True")
-  ;;   (setq python-shell-interpreter "ipython"
-  ;; 	python-shell-prompt-detect-failure-warning nil)
-  ;;   (add-to-list 'python-shell-completion-native-disabled-interpreters
-  ;; 	       "ipython")
+	"-i --simple-prompt --InteractiveShell.display_page=True"
+	python-shell-prompt-detect-failure-warning nil)
+  (add-to-list 'python-shell-completion-native-disabled-interpreters
+	       "ipython")
 
   ;; Enable Flycheck
   (when (require 'flycheck nil t)
@@ -251,13 +332,49 @@
   :ensure auctex)
 (add-hook 'tex-mode-hook 'lsp-mode)
 (add-hook 'tex-mode-hook 'flycheck-mode)
+;; Use LatexMK for compiling and inheret pdf setting from auctex
+(use-package auctex-latexmk
+:ensure t
+:config
+(auctex-latexmk-setup)
+(setq auctex-latexmk-inherit-TeX-PDF-mode t)
+)
+;; Use RefTeX for citations and references
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t)
+;; Use helm-bibtex for references and citations
+;; (use-package helm
+;; :ensure t)
+;; (use-package helm-bibtex
+;; :ensure t)
+;; (autoload 'helm-bibtex "helm-bibtex" "" t)
+
+;; Use ivy-bibtex for citations and references
+;; (use-package ivy-bibtex
+;;   :ensure t
+;;   :config
+;;   (autoload 'ivy-bibtex "ivy-bibtex" "" t)
+;;   ;; ivy-bibtex requires ivy's `ivy--regex-ignore-order` regex builder, which
+;;   ;; ignores the order of regexp tokens when searching for matching candidates.
+;;   ;; Add something like this to your init file:
+;;   (setq ivy-re-builders-alist
+;; 	  '((ivy-bibtex . ivy--regex-ignore-order)
+;; 	    (t . ivy--regex-plus)))
+;;   ;; Change default action to insert citation from open URL
+;;   (setq ivy-bibtex-default-action 'ivy-bibtex-insert-citation)
+;;   (setq ivy-bibtex-default-multi-action 'ivy-bibtex-insert-key)
+;;   (setq bibtex-completion-cite-prompt-for-optional-arguments nil)
+;;   :bind
+;;   (
+;;    ("C-c c" . ivy-bibtex-with-local-bibliography)
+;;    )
+;;   )
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ignored-local-variable-values '((TeX-command-extra-options . "-shell-escape")))
  '(package-selected-packages
    '(helpful smex rainbow-delimiters ivy-rich doom-modeline dashboard julia-mode julia-repl orderless magit evil-leader counsel ivy org which-key vertico use-package evil doom-themes)))
 (custom-set-faces
